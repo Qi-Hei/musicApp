2d20df700759806bef583967accbb076b071ae77
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-20 22:26:35 +0800

    1

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+   private ImageView nextIV,playIV,lastIV,albumIV,menuIV;
+   private TextView singerTV,songTV;
+   private SearchView musicSearch;
+   private RecyclerView musicRV;
+   private List<LocalMusicBean> MainData, SetData;
+   private LocalMusicAdapter MusicAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
