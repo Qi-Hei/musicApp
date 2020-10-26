bd81ce72c44ea76b49c030bf358e510719032b6a
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-26 14:57:46 +0800

    1

--- app/src/main/java/com/example/musicapp/LocalMusicAdapter.java
+++ app/src/main/java/com/example/musicapp/LocalMusicAdapter.java
@@ -1,4 +1,62 @@
 package com.example.musicapp;
 
-public class LocalMusicAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class LocalMusicAdapter extends RecyclerView.Adapter<LocalMusicAdapter.LocalMusicViewHolder>{
+    Context context;
+    List<LocalMusicBean> mDatas;
+
+    OnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener{
+        public void OnItemClick(View view, int position);
+    }
+
+    public LocalMusicAdapter(Context context, List<LocalMusicBean> mDatas) {
+        this.context = context;
+        this.mDatas = mDatas;
+    }
+
+
+    @NonNull
+    @Override
+    public LocalMusicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LocalMusicViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class LocalMusicViewHolder extends RecyclerView.ViewHolder{
+        TextView idTv,songTv,singerTv,albumTv,timeTv;
+        public LocalMusicViewHolder(View itemView) {
+            super(itemView);
+            idTv = itemView.findViewById(R.id.music_num);
+            songTv = itemView.findViewById(R.id.);
+            singerTv = itemView.findViewById(R.id.music_singer);
+            albumTv = itemView.findViewById(R.id.music_album);
+            timeTv = itemView.findViewById(R.id.music_durtion);
+        }
+    }
+
 }
