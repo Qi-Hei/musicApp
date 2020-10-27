b80e998b3f1337d2fe9ff6b25d4d65fe37ca8bdc
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-27 10:04:48 +0800

    1

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.BroadcastReceiver;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
@@ -21,8 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
    //与服务有关的
     private  int musicDataSize;
-    private int currentId = 2;
+    private int currentId = -2;
     private musicService.MyBinder myBinder;
+    private ServiceConnection serviceConnection;
+    private Handler handler;
+    private SeekBar seekBar;
+    private Runnable runnable;
+    private BroadcastReceiver myReceiver;
+    private boolean isNetAvailable = false;
+
+    //侧滑栏部分
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private Fragment fragment_about, fragment_introduction, fragment_message;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+    private int Notification_height;
 
 
 
